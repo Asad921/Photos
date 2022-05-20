59fbcc5e0c8a1880b2e7d61c993ae990b054565c
Asad921 <officiallasad.ali@gmail.com>
2022-05-20 16:58:29 +0500

    commit

--- src/Models/SampleImage.php
+++ src/Models/SampleImage.php
@@ -5,7 +5,7 @@ namespace Photos;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Image extends Model
+class SampleImage extends Model
 {
     use HasFactory;
 }
